4e47524b76429afc36a7429112883bd645407188
Nikos1010 <nimoba101@gmail.com>
2024-07-05 16:52:11 -0500

    Added: translations

--- _i18n/en-us/index.js
+++ _i18n/en-us/index.js
@@ -1,3 +1,33 @@
 export default {
-  "Iaccounting": {}
+  "Iaccounting": {
+    'sidebar': {
+      'adminGroup': 'Accounting',
+      'adminPurchases': 'Expense'
+    },
+    'title': {
+      'analyzeDocument': 'Upload Your Document for Analysis',
+      'verifyDocument': 'Verify Your Document',
+    },
+    'label': {
+      'documentSupport': 'Support Document',
+      'electronicInvoice': 'Electronic Invoice'
+    },
+    'form': {
+      'elaborationDate': 'Elaboration Date',
+      'total': 'Total',
+      'totalTax': 'Total Tax',
+      'currencyCode': 'Currency Code',
+      'discount': 'Total Discount',
+      'subtotal': 'Subtotal',
+      'documentType': 'Document Type',
+      'providerName': 'Provider Name',
+      'providerIdNumber': 'Provider Identification',
+      'providerIdType': 'Type of Provider Identification',
+      'documentAnalysis': 'Document for Analysis'
+    },
+    'messages': {
+      'descriptionAnalyze': 'Upload your files in PDF, image, or Word format (supporting documents, invoices, receipts, etc.) so we can analyze them. Make sure the file is legible.',
+      'descriptionValidate': 'Please carefully review the information below and make sure everything is correct. If you find any errors, correct them before proceeding.'
+    }
+  }
 }
--- _i18n/es/index.js
+++ _i18n/es/index.js
@@ -1,3 +1,36 @@
 export default {
-  "Iaccounting": {}  
-}
+  "Iaccounting": {
+    'sidebar': {
+      'adminGroup': 'Contabilidad',
+      'adminPurchases': 'Gastos'
+    },
+    'title': {
+      'uploadDocument': 'Sube tu Documento para Analizar',
+      'verifyDocument': 'Verifica tu Documento',
+    },
+    'button': {},
+    'table': {},
+    'label': {
+      'documentSupport': 'Documento de Soporte',
+      'electronicInvoice': 'Factura Electronica'
+    },
+    'form': {
+      'elaborationDate': 'Fecha de elaboración',
+      'total': 'Total',
+      'subtotal': 'Subtotal',
+      'totalTax': 'Total Impuestos',
+      'currencyCode': 'Codigo de Moneda',
+      'discount': 'Total Descuento',
+      'documentType': 'Tipo de Documento',
+      'providerName': 'Nombre del Proveedor',
+      'providerIdNumber': 'Identificación del Proveedor',
+      'providerIdType': 'Tipo de Identificación del Proveedor',
+      'documentAnalysis': 'Documento para Analizar'
+    },
+    'messages': {
+      'descriptionAnalyze': 'Sube tus archivos en formato PDF, imagen o word (soportes, facturas, recibos, etc.) para que podamos analizarlos. Asegúrate de que el archivo sea legible.',
+      'descriptionValidate': 'Por favor, revisa cuidadosamente la información que aparece a continuación y asegúrate de que todo sea correcto. Si encuentras algún error, corrígelo antes de continuar.'
+    },
+    'validation': {},
+  }
+};
